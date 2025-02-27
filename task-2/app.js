10e56991451d0be9208d837a54f457a4287a657a
grfatime <kurbanovhafatime@gmail.com>
2025-02-28 01:53:30 +0400

    Konsola N dəfə «I know how to use cycles» mesajı çıxaran proqram yazın.

--- task-2/app.js
+++ task-2/app.js
@@ -1,10 +1,10 @@
 let N = +prompt("Nece defe cixarmaq isteyirsiniz?");
 
-
+let i;
 
 if ( N > 0 ) {
 
-    for (let i = 0 ; i < N ; i++) { 
+    for (i = 0; i < N; i++) { 
         console.log("I know how to use cycles");
     }
 
