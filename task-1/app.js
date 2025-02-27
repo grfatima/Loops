b81d2bc7fc1c94347774404bc153bc9f54bfac26
grfatime <kurbanovhafatime@gmail.com>
2025-02-28 00:11:04 +0400

    İkirəqəmli ədədi üçrəqəmli olana qədər artırın.

---
+++ task-1/app.js
@@ -0,0 +1,13 @@
+let i = +prompt("Iki reqemli eded daxil edin.")
+
+if (i >= 10 && i < 100) {
+
+    for (i; i < 100 ; i += 7){
+        console.log(i)
+    }
+
+} else {
+    console.log("Ikireqemli bir eded daxil edin.")
+}
+
+
